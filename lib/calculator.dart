98365625bee9f2b9ad5fe6041483acfb56008371
RAJENDRA SINGH BISHT <rajanvisa2001@gmail.com>
2026-01-04 13:21:12 +0530

    feat: support \n new lines between numbers

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -3,6 +3,6 @@ int add(String number) {
     return 0;
   }
 
-  final parts = number.split(',');
+  final parts = number.split(RegExp(r'[,\n]'));
   return parts.map(int.parse).reduce((a, b) => a + b);
 }
