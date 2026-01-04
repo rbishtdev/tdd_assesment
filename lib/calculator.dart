5a1336150964ae2539b24aab1001f635da93c1d8
RAJENDRA SINGH BISHT <rajanvisa2001@gmail.com>
2026-01-04 13:02:34 +0530

    feat: supports two comma-separated numbers

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -2,5 +2,11 @@ int add(String number) {
   if(number.isEmpty) {
     return 0;
   }
-  return int.parse(number);
+
+  final parts = number.split(',');
+  if (parts.length == 1) {
+    return int.parse(parts[0]);
+  }
+
+  return int.parse(parts[0]) + int.parse(parts[1]);
 }
