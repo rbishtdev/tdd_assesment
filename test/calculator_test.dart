a3be22944601c6fc17886c2e3e5087b33efd1542
RAJENDRA SINGH BISHT <rajanvisa2001@gmail.com>
2026-01-04 12:49:19 +0530

    test: add returns 0 for empty string

--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -2,7 +2,7 @@ import 'package:calculator/calculator.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('return 0 for empty string', () {
+    expect(add(''), 0);
   });
 }
