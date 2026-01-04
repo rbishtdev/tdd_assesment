54a69313c5ea1e7af65b9a818229429c5182c2f8
RAJENDRA SINGH BISHT <rajanvisa2001@gmail.com>
2026-01-04 13:16:55 +0530

    test: add supports multiple comma-separated numbers

--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -13,4 +13,8 @@ void main() {
   test('return sum of two comma-separated numbers', () {
     expect(add('1,3'), 4);
   });
+
+  test('supports multiple numbers', () {
+    expect(add('1,2,3,4,5,6,7,8,9,10'), 55);
+  });
 }
