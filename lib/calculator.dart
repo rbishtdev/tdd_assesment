2f9f822d192d6b5eb6424a9e34b44ae509a429b8
RAJENDRA SINGH BISHT <rajanvisa2001@gmail.com>
2026-01-04 13:26:30 +0530

    feat: parse different delimiter

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -3,6 +3,15 @@ int add(String number) {
     return 0;
   }
 
-  final parts = number.split(RegExp(r'[,\n]'));
+  String delimiterPattern = r'[,\n]';
+  String numberSection = number;
+
+  if (number.startsWith('//')) {
+    final lines = number.split('\n');
+    delimiterPattern = RegExp.escape(lines[0].substring(2));
+    numberSection = lines[1];
+  }
+
+  final parts = numberSection.split(RegExp(delimiterPattern));
   return parts.map(int.parse).reduce((a, b) => a + b);
 }
