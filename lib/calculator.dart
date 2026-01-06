28a19f79f4719146aa6bab93f829b3b5f46669cd
RAJENDRA SINGH BISHT <rajanvisa2001@gmail.com>
2026-01-06 17:20:56 +0530

    refactor: remove DelimiterParser class

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -1,12 +1,10 @@
-import 'delimeter_parser.dart';
-
 int add(String number) {
-  if(number.isEmpty) {
+  if (number.isEmpty) {
     return 0;
   }
 
-  final delimiterPattern = DelimiterParser.extractPattern(number);
-  final numberSection = DelimiterParser.extractNumberSection(number);
+  final delimiterPattern = extractPattern(number);
+  final numberSection = extractNumberSection(number);
 
   final values = numberSection
       .split(RegExp(delimiterPattern))
@@ -15,12 +13,28 @@ int add(String number) {
 
   final negatives = values.where((n) => n < 0).toList();
   if (negatives.isNotEmpty) {
-    throw Exception(
-      'negative numbers not allowed ${negatives.join(',')}',
-    );
+    throw Exception('negative numbers not allowed ${negatives.join(',')}');
   }
 
-  return values
-      .where((n) => n <= 1000)
-      .fold(0, (sum, n) => sum + n);
+  return values.where((n) => n <= 1000).fold(0, (sum, n) => sum + n);
+}
+
+const String defaultPattern = r'[,\n]';
+
+String extractPattern(String input) {
+  if (!input.startsWith('//')) {
+    return defaultPattern;
+  }
+
+  final delimiterLine = input.split('\n').first;
+
+  if (delimiterLine.startsWith('//[')) {
+    return RegExp.escape(delimiterLine.substring(3, delimiterLine.length - 1));
+  }
+
+  return RegExp.escape(delimiterLine.substring(2));
+}
+
+String extractNumberSection(String input) {
+  return input.startsWith('//') ? input.split('\n')[1] : input;
 }
